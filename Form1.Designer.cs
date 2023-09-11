9a9ffab8be38b694116bd22451806c1e16c835aa
ADMIN <ADMIN@LAB1-MAY19>
2023-09-11 16:33:45 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace CSharp_bai9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NAME = new System.Windows.Forms.Label();
+            this.RAM = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNAME = new System.Windows.Forms.TextBox();
+            this.txtRAM = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(148, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1181, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chương Trình Lấy Thông Tin Máy Tính ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NAME
+            // 
+            this.NAME.AutoSize = true;
+            this.NAME.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NAME.Location = new System.Drawing.Point(15, 167);
+            this.NAME.Name = "NAME";
+            this.NAME.Size = new System.Drawing.Size(132, 39);
+            this.NAME.TabIndex = 1;
+            this.NAME.Text = "NAME:";
+            // 
+            // RAM
+            // 
+            this.RAM.AutoSize = true;
+            this.RAM.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RAM.Location = new System.Drawing.Point(27, 319);
+            this.RAM.Name = "RAM";
+            this.RAM.Size = new System.Drawing.Size(108, 39);
+            this.RAM.TabIndex = 2;
+            this.RAM.Text = "RAM:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button1.Location = new System.Drawing.Point(597, 467);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(372, 114);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "THOÁT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNAME
+            // 
+            this.txtNAME.Location = new System.Drawing.Point(317, 176);
+            this.txtNAME.Name = "txtNAME";
+            this.txtNAME.Size = new System.Drawing.Size(545, 20);
+            this.txtNAME.TabIndex = 4;
+            // 
+            // txtRAM
+            // 
+            this.txtRAM.Location = new System.Drawing.Point(317, 337);
+            this.txtRAM.Name = "txtRAM";
+            this.txtRAM.Size = new System.Drawing.Size(545, 20);
+            this.txtRAM.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1658, 616);
+            this.Controls.Add(this.txtRAM);
+            this.Controls.Add(this.txtNAME);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RAM);
+            this.Controls.Add(this.NAME);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label NAME;
+        private System.Windows.Forms.Label RAM;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNAME;
+        private System.Windows.Forms.TextBox txtRAM;
+    }
+}
+
